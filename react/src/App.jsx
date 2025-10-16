c25e6f1ac268e2276b8ebc5232e3b57edc530388
Erol <e_pehlivan@windowslive.com>
2025-10-16 21:07:31 +0100

    react crash course

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+rafce
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import './App.css'
+import { useState, useEffect} from 'react'
+ 
+const Card = ({title}) => {
+  const [ count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+useEffect(() => {
+console.log(`${title} has been liked: ${hasLiked}`);
+}, [hasLiked]);
+
+useEffect( ()   => {
+console.log('Card Rendered')
+
+}, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+<div className="card" onClick={() => setCount((prevState) => prevState +1 )
+}>
+  <h2>{title} - {count || null} </h2>
+
+  <button onClick= {() => setHasLiked(!hasLiked)}>
+    {hasLiked ? 'Liked' : 'Like'}
+  </button>
+</div>
+
   )
 }
 
-export default App
+
+
+
+const App = () => {
+
+  return (
+    <div className='card-container'>
+    <Card title="Star Wars" rating={5} isCool={true} />
+    <Card title="Avatar" />
+    <Card title="The Lion King" />
+    </div>
+  )
+}
+export default App */
