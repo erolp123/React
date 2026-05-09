48c0ce9857549d227e3af2d3cca66759176eee7d
Erol <e_pehlivan@windowslive.com>
2026-05-10 00:47:43 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,121 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "./Components/Search";
+import Spinner from "./Components/Spinner";
+import MovieCard from "./Components/MovieCard";
+import {useDebounce} from 'react-use'
+
+// API - Application Programming Interface - a set of rules that allows one
+// software application to talk to another
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieLiest, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+
+
+  // Debounce the search term to prevent making too many API requests
+  // by waiting for the user to stop typing for 500ms
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+
   
+  const fetchMovies = async (query =' ') => {
+    
+    try {
+      const endpoint = query 
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` 
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+
+      if (data.Response == "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results || []);
+
+      console.log(data);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage("Error fetching movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
   return (
     <main>
-<div className="pattern" />
+      <div className="pattern" />
 
-<div className="wrapper" />
-<header>
-  <img src="./hero.png" alt="Hero Banner"/>
-      <h1> Find <span className="text-gradient">Movies</span> You'll Enjoy Without the hassle </h1>
-</header>
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            {" "}
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the hassle{" "}
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
 
-<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <section className="all-movies">
+          <h2 className="mt-[40]">All Movies</h2>
 
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieLiest.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
     </main>
   );
 };
 
-const url = 'https://imdb236.p.rapidapi.com/api/imdb/cast/nm0000190/titles';
+const url = "https://imdb236.p.rapidapi.com/api/imdb/cast/nm0000190/titles";
 const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'c30cbc4b0emshab85b37801fc293p1c8dbejsnbda485768cfd',
-		'x-rapidapi-host': 'imdb236.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "c30cbc4b0emshab85b37801fc293p1c8dbejsnbda485768cfd",
+    "x-rapidapi-host": "imdb236.p.rapidapi.com",
+  },
 };
 
 try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
+  const response = await fetch(url, options);
+  const result = await response.text();
+  console.log(result);
 } catch (error) {
-	console.error(error);
+  console.error(error);
 }
 
 export default App;
